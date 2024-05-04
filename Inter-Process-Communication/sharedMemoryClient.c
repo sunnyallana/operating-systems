812df06fd0a11c4ea43d0da26df499378e7cefea
Sunny Shaban Ali <study.sunnyallana@gmail.com>
2024-05-04 15:16:15 +0500

    Update sharedMemoryClient.c

--- Inter-Process-Communication/sharedMemoryClient.c
+++ Inter-Process-Communication/sharedMemoryClient.c
@@ -3,29 +3,39 @@
 #include <sys/shm.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h> // Include ctype.h for the toupper and tolower functions
 
 #define MAX_SIZE 27
 
 int main(){
-	int shmid;
-	key_t key;
-	char* shm, *s;
+    int shmid;
+    key_t key;
+    char* shm, *s;
+    
+    key = 2211;
+    
+    if((shmid = shmget(key, MAX_SIZE, IPC_CREAT | 0666)) < 0){ // Corrected the syntax
+        perror("shmget");
+        exit(1);
+    }
+    
+    if((shm = shmat(shmid, NULL, 0)) == (char*)-1){ // Corrected the syntax
+        perror("shmat");
+        exit(1);
+    }
+    s = shm;
+    
+    for(s = shm; *s != '\0'; s++){
+        if(islower(*s))
+            putchar(toupper(*s));
+        else if(isupper(*s))
+            putchar(tolower(*s));
+        else
+            putchar(*s);
+    }
+    putchar('\n');
 	
-	key = 2211;
+    *shm = '\0';
 	
-	if(shmid = shm_get(key, MAX_SIZE, IPC_CREAT | 0666) < 0){
-		return 1;
-	}
-	
-	if(shm = shm_at(shmid, NULL, 0) == (char*)-1){
-		return 1;
-	}
-	s = shm;
-	
-	for(s = shm; *s != '\0'; s++){
-		putchar(*s);
-	}
-	*shm = '\0';
-	puts("");
-	exit(0);	
+    exit(0);    
 }
