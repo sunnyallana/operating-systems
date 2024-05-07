c9fe644dcbb491e4bfd967a3f3a4c7f502bdf87c
Sunny Shaban Ali <study.sunnyallana@gmail.com>
2024-05-07 20:02:35 +0500

    Update chimpanzeeCrossingTheRope.c

--- Threads-Synchronization/chimpanzeeCrossingTheRope.c
+++ Threads-Synchronization/chimpanzeeCrossingTheRope.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <semaphore.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+sem_t semaphore;
 int crossing = 0;
 
 void* chimp(void* arg) {
     int id = *(int*)arg;
 
     while (1) {
-        pthread_mutex_lock(&mutex);
+        sem_wait(&semaphore);
 
         while (crossing != 0 && crossing != id) {
-            pthread_cond_wait(&cond, &mutex);
+            sem_post(&semaphore);
+            sem_wait(&semaphore);
         }
 
         crossing = id;
         printf("Chimpanzee %d is crossing.\n", id);
         fflush(stdout);
 
-        pthread_mutex_unlock(&mutex);
+        sem_post(&semaphore);
         usleep(1000000);
 
-        pthread_mutex_lock(&mutex);
+        sem_wait(&semaphore);
         crossing = 0;
         printf("Chimpanzee %d has crossed.\n", id);
         fflush(stdout);
 
-        pthread_cond_broadcast(&cond);
-        pthread_mutex_unlock(&mutex);
+        sem_post(&semaphore);
 
         usleep(1000000);
     }
@@ -41,11 +41,15 @@ int main() {
     pthread_t chimp1, chimp2;
     int ids[2] = {1, 2};
 
+    sem_init(&semaphore, 0, 1);
+
     pthread_create(&chimp1, NULL, chimp, &ids[0]);
     pthread_create(&chimp2, NULL, chimp, &ids[1]);
 
     pthread_join(chimp1, NULL);
     pthread_join(chimp2, NULL);
+
+    sem_destroy(&semaphore);
 
     return 0;
 }
