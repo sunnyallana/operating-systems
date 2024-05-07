ac142595727fd64fb156746b9b5e95f6cd41a7c1
Sunny Shaban Ali <study.sunnyallana@gmail.com>
2024-05-07 20:40:04 +0500

    Update trivia.c

--- Threads-Synchronization/trivia.c
+++ Threads-Synchronization/trivia.c
@@ -3,13 +3,12 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
+#include <semaphore.h>
 
 #define NUM_PARTICIPANTS 4
 #define MAX_QUESTIONS 10
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-int current_participant = 0;
+sem_t semaphores[NUM_PARTICIPANTS];
 int scores[NUM_PARTICIPANTS] = {0};
 
 void* participant_function(void* arg) {
@@ -17,22 +16,15 @@ void* participant_function(void* arg) {
     int questions_answered = 0;
 
     while (questions_answered < MAX_QUESTIONS) {
-        pthread_mutex_lock(&mutex);
-
-        while (current_participant != id) {
-            pthread_cond_wait(&cond, &mutex);
-        }
+        sem_wait(&semaphores[id]);
 
         int is_correct = (rand() % 2 == 0) ? 1 : 0;
         if (is_correct) {
             scores[id]++;
         }
 
-        current_participant = (current_participant + 1) % NUM_PARTICIPANTS;
         questions_answered++;
-        pthread_cond_broadcast(&cond);
-
-        pthread_mutex_unlock(&mutex);
+        sem_post(&semaphores[(id + 1) % NUM_PARTICIPANTS]);
     }
 
     return NULL;
@@ -43,6 +35,11 @@ int main() {
     int participant_ids[NUM_PARTICIPANTS] = {0, 1, 2, 3};
 
     srand(time(NULL));
+
+    for (int i = 0; i < NUM_PARTICIPANTS; i++) {
+        sem_init(&semaphores[i], 0, 0);
+    }
+    sem_post(&semaphores[0]); // Start with the first participant
 
     for (int i = 0; i < NUM_PARTICIPANTS; i++) {
         pthread_create(&participants[i], NULL, participant_function, &participant_ids[i]);
@@ -67,6 +64,10 @@ int main() {
         printf("Participant %d | Score: %d\n", i, scores[i]);
     }
     printf("Winner: Participant %d with score %d\n", winner_id, max_score);
+
+    for (int i = 0; i < NUM_PARTICIPANTS; i++) {
+        sem_destroy(&semaphores[i]);
+    }
 
     return 0;
 }
